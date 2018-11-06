fb198a058ef020149cf8405d212c5ac3d42ecaf8
dragonpym <44492111+dragonpym@users.noreply.github.com>
2018-11-06 22:50:56 +0800

    Update 合并排序.py

--- 168206121/合并排序.py
+++ 168206121/合并排序.py
@@ -1,30 +1,32 @@
-def sorf(a,b):
-	c = []
-	h = j = 0
-	while j<len(a) and h<len(b):
-		if a[j] < b[h]:
-			c.append(a[j])
-			j += 1
-		else:
-			c.append(b[h])
-			h += 1
-	if j==len(a):
-		for i in b[h:]:
-			c.append(i)
+	def Message(arrays,left,eight):
+	mid = len(arrays)/2
+	mid = int(mid)
+	if left == mid or mid == eight:
+		if int(arrays[left-1]) > int(arrays[eight-1]):
+			arrays[left-1],arrays[eight-1] = arrays[eight-1],arrays[left-1]
+		return arr
 	else:
-		for i in a[j:]:
-			c.append(i)
-	return c
+		arrays1 = Message(arrays[:mid],left,mid)
+		a = int(eight) - int(mid)
+		arrays2 = Message(arrays[mid:],left,a)
+		return sorf(arrays1,arrays2)
+
+def sorf(arr1,arr2):
+	arr = []
+	while len(arr1)!=0 and len(arr2)!=0:
+		if arr1[0] >= arr2[0]:
+			arr.append(arr2.pop(0))
+		else:
+			arr.append(arr1.pop(0))
+	if len(arr1)==0:
+		for i in range(0,int(len(arr2))):
+			arr.append(arr2[i])
+	else:
+		for i in range(0,int(len(arr1))):
+			arr.append(arr1[i])
+	return arr
 	
-def message(lis):
-	if len(lis) <= 1:
-		return lis
-	mid = int(len(lis)/2)
-	left = message(lis[:mid])
-	eight = message(lis[mid:])
-	return sorf(left,eight)
-	
-a = [15,45,88,65,21,45,7,56,15,54]
-b = message(a)
-print(a)
-print(b)		
+arrge = [1,5,2,3,4,8,7,6]
+arrge2 = Message(arrge,1,int(len(arrge)))
+print(arrge)
+print(arrge2)	
