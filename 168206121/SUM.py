b9b40d897df8de90a9d541958d30c2262570029a
dragonpym <44492111+dragonpym@users.noreply.github.com>
2018-11-06 22:46:17 +0800

    Update SUM.py

--- 168206121/SUM.py
+++ 168206121/SUM.py
@@ -1,20 +1,7 @@
-"""
-def sum (alist):
-	s = 0
-	if len(alist)==0:
+def SUM(ali):
+	if len(ali)==0:
 		return 0
-	else:
-		s += alist[0]
-		alist.remove(alist[0])
-		return s + sum(alist)
-"""
-def Sum(alist):
-	if len(alist) < 1:
-		return 0
-	else:
-		for i in alist:
-			aum += i
-			return sum
-alist = [2,3,4,5]
-s=sum(alist)
-print(s)
+	return ali[0] + SUM(ali[1:])
+alist = [29,3,,32,6,4,5]
+a=sum(ali)
+print(a)
