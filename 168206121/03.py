18a14b681131428323ab0c5cd4e846fd11b493ff
dragonpym <44492111+dragonpym@users.noreply.github.com>
2018-11-06 22:54:05 +0800

    Update and rename 快速排序.py to 03.py

---
+++ 168206121/03.py
@@ -0,0 +1,10 @@
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <=pivot]
+        greater = [i for i in array[1:] if i >pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+arr = [6,45,68,15,34,56,78,5,94,646,642,54,8,78,54,64,315,354,46,89,97]
+print (quicksort(arr))  
